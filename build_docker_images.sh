5a2e3a97ce8e0996c5f7b4c68a3d46f407e722b6
Thalles Rodrigues <ts.rodr@gmail.com>
2024-08-27 22:59:28 -0300

    tweaking scripts

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-docker build --tag sisdrenagem/db ./sisdrenagem-db/
-docker build --tag sisdrenagem/api ./sisdrenagem-api/
-docker build --tag sisdrenagem/ui ./sisdrenagem-ui/
+CURR_DIR=$PWD
+UI_GIT_REPO=refactor
+API_GIT_REPO=database-integration
+
+cd $PWD/Back
+git checkout $API_GIT_REPO && git pull
+cd $PWD/Front
+git checkout $UI_GIT_REPO && git pull
+cd $PWD
+
+docker build --tag sisdrenagem/db ./postgres/
+docker build --tag sisdrenagem/api ./Back/
+docker build --tag sisdrenagem/ui ./Front/
