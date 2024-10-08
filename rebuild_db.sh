b7a67537b1f372d0a5377ae8c3aa9345bd38b5c3
Thalles Rodrigues <ts.rodr@gmail.com>
2024-10-08 13:56:01 -0300

    added rebuild_db, user signup

---
+++ rebuild_db.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker stop sisdrenagem-db
+docker container remove sisdrenagem-db
+docker image remove sisdrenagem-db
+docker build --tag sisdrenagem/db ./postgres/
+docker run --name sisdrenagem-db \
+  -e POSTGRES_USER=sisdrenagem \
+  -e POSTGRES_PASSWORD=tartarugasninja \
+  -e POSTGRES_DB=sisdrenagem-dev \
+  -d sisdrenagem/db:latest
