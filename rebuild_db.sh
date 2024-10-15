36cfdc3539933eee64ef84d7c59bf094e3795850
Thalles Rodrigues <ts.rodr@gmail.com>
2024-10-15 15:29:52 -0300

    basic login working

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -5,6 +5,7 @@ docker container remove sisdrenagem-db
 docker image remove sisdrenagem-db
 docker build --tag sisdrenagem/db ./postgres/
 docker run --name sisdrenagem-db \
+  -e POSTGRES_HOST=sisdrenagem-db \
   -e POSTGRES_USER=sisdrenagem \
   -e POSTGRES_PASSWORD=tartarugasninja \
   -e POSTGRES_DB=sisdrenagem-dev \
