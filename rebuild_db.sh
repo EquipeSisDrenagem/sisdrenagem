8d3a891fb254e9bb892d21fb5a505f325263d5bf
Thalles Rodrigues <ts.rodr@gmail.com>
2024-10-17 13:31:16 -0300

    temp fix for rebuild_db

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -4,9 +4,6 @@ docker stop sisdrenagem-db
 docker container remove sisdrenagem-db
 docker image remove sisdrenagem-db
 docker build --tag sisdrenagem/db ./postgres/
-docker run --name sisdrenagem-db \
-  -e POSTGRES_HOST=sisdrenagem-db \
-  -e POSTGRES_USER=sisdrenagem \
-  -e POSTGRES_PASSWORD=tartarugasninja \
-  -e POSTGRES_DB=sisdrenagem-dev \
-  -d sisdrenagem/db:latest
+docker compose up -d
+# docker run --name sisdrenagem-db -e POSTGRES_HOST=sisdrenagem-db -e POSTGRES_USER=sisdrenagem -e POSTGRES_PASSWORD=tartarugasninja -e POSTGRES_DB=sisdrenagem-dev -d sisdrenagem/db:latest
+
